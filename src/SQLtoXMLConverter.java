0a9b60e8f9ea53b6ff875501f39092db3f910768
Brandon White <bmwqg@live.com>
2016-11-15 22:08:22 -0500

    Added add'l classes -- SQLParser and SQLtoXMLConverter

---
+++ src/SQLtoXMLConverter.java
@@ -0,0 +1,12 @@
+
+// main class wrapper for entire project
+public class SQLtoXMLConverter {
+	
+	public static void main(String[] args){
+		
+		SQLParser parser = new SQLParser();
+		parser.close();	// close database connection after no longer necessary
+		
+	}
+
+}
