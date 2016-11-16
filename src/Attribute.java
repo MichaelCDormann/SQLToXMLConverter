cd6159cc44efb5dae01c1e83dd572c3065ea97ec
Brandon White <bmwqg@live.com>
2016-11-15 21:22:12 -0500

    Corrected issue with Eclipse Project path not being added correctly. Added Attribute and Group class in src path.

---
+++ src/Attribute.java
@@ -0,0 +1,9 @@
+
+public class Attribute {
+	
+	String name;		// attribute's original name (attribute name in table)
+	String alias;		// attribute's alias (attribute name upon query)
+	Group group;		// group object if the attribute is to be grouped
+	String tableName;	// tableName that the attribute belongs to
+
+}
---
+++ src/Group.java
@@ -0,0 +1,9 @@
+
+// group class used for grouping (DEFINITION #3)
+public class Group {
+	
+	String name;		// name of group <THIS-RIGHT-HERE, attr1, attr2>
+	boolean compFlag;	// is this group compressed?
+	Attribute compTo;	// the attribute information for the group compression
+	
+}
