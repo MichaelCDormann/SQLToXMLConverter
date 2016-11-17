2235343d729a9e91e297409ff080137d55322629
Collin M <n00871920@unf.edu>
2016-11-17 13:25:07 -0500

    Added a XMLformat Class to handle XML Formatting

---
+++ src/xmlFormat.java
@@ -0,0 +1,15 @@
+/*
+ * XML Formatter
+ * Will be passed a ResultSet object from a selection query
+ * Will be passed ArrayList of Attribute object
+ * 
+ * 
+ */
+
+public class xmlFormat {
+
+	
+	
+	
+	
+}
