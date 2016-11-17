fa7a151fad7ce1a060c2ebe61434979e481b2eba
Collin M <n00871920@unf.edu>
2016-11-17 13:43:33 -0500

    made a journal for Collin & David, Also added imports needed to XML Class

--- src/xmlFormat.java
+++ src/xmlFormat.java
@@ -3,11 +3,21 @@
  * Will be passed a ResultSet object from a selection query
  * Will be passed ArrayList of Attribute object
  * 
+ * Will print the correct out put
  * 
  */
 
+import java.sql.*;
+import java.io.*;
+import java.util.ArrayList;
+
 public class xmlFormat {
 
+public static void XML(ResultSet rSet, ArrayList<Attribute> aList)
+{
+	
+	
+}
 	
 	
 	
