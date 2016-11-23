d47bbd15f5ef8f10f6e5ec7f69987ee9e7b21282
Brandon White <bmwqg@live.com>
2016-11-22 19:01:01 -0500

    corrected regex for tokenizer, parser started, created parseException

---
+++ src/ParseException.java
@@ -0,0 +1,12 @@
+// custom parse exception class
+public class ParseException extends Exception {
+	
+	private static final long serialVersionUID = -4922810844499906083L;
+
+	public ParseException(String message){
+		
+		super(message);
+		
+	}
+
+}
--- src/SQLParser.java
+++ src/SQLParser.java
@@ -1,23 +1,155 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Stack;
+import java.util.regex.Pattern;
 
 public class SQLParser {
 	
-	Connection conn;	// database connection object
-
-	// parser constructor
-	SQLParser(){
-		
-	}
+	Connection conn;				// database connection object
+	ArrayList<String> tokenList;	// list of tokens from SQL Query
+	String generatedQuery;			// query that is generated throughout the states
 	
 	
 	public void parseQuery(String query){
 		
-		ArrayList<String> tokenList = new ArrayList<String>();
-		Tokenizer t = new Tokenizer(tokenList);
+		generatedQuery = "";
+		
+		Stack<Group> groupStack = new Stack<Group>();
+		
+		this.tokenList = new ArrayList<String>();
+		Tokenizer t = new Tokenizer(this.tokenList);
 		
 		t.tokenize(query);	// tokenizes query and updates tokenList with tokens
 		
-	}	
+		// call start method -- state 1 of FSM
+		startState();
+		
+		System.out.println(generatedQuery);
+		
+	}
+	
+	
+	
+	
+	// update query with new element
+	public void addQuery(String s){
+		this.generatedQuery += " " + s + " ";
+	}
+	
+	
+	
+	
+	
+	// verifies nextToken matches and removes from the list
+	public boolean isNextToken(String s){
+		
+		if(this.tokenList.get(0).equals(s)){
+			this.tokenList.remove(0);
+			return true;
+		}else{
+			return false;
+		}
+		
+	}
+	
+	
+	
+	
+	
+	// verifies if nextToken matches an ID
+	// used for attribute and tables, including attributes with table names preceding
+	public boolean nextIsID(){
+		
+		if(this.tokenList.get(0).matches("[A-Za-z][A-Za-z0-9_$#]*(.[A-Za-z0-9_$#]+)?")){
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+	
+	
+	
+	// verifies if nextToken matches a group name
+	// used for group name checks within group markers
+	public boolean nextIsGroupName(){
+		
+		if(this.tokenList.get(0).matches("")){
+			return true;
+		}
+		
+		return false;
+		
+	}
+	
+	
+	
+	
+	/*
+	 *  STATES      |   METHOD
+	 * -------------|-----------
+	 * State  1		|    startState
+	 * State  2		|    attributeState
+	 * State  3		| 
+	 * State  4		|
+	 * State  5		|
+	 * State  6		|
+	 * State  7		|
+	 * State  8		|
+	 * State  9		|
+	 * State 10		|
+	 * State 11		|
+	 * State 12		|
+	 * State 13		|
+	 * State 14		|
+	 * State 15		|
+	 */
+	
+	// within state 3, 7, 12 handle AS keywords
+	// after as keyword is handled, create attribute and
+	// add to ArrayList then remove from tokenList
+	
+	// initial start state 
+	public void startState(){
+		
+		if(isNextToken("select")){
+			addQuery("select");
+			//call 2
+			attributeState();
+		}else{
+			
+			try {
+				throw new ParseException("SELECT statement not found");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	} // end start state
+	
+	// state 2
+	public void attributeState(){
+		
+		if(isNextToken("*")){
+			addQuery("*");
+			// call 5
+		}else if(nextIsID()){
+			// call 3
+		}else{
+			
+			try {
+				throw new ParseException("Attributes not specified");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+	}
 	
 }
