03e80e3223cb547221be199eab576a056b545f14
FlyingDavi <david.a.timmers@gmail.com>
2016-11-25 14:10:03 -0500

    Added commenting and formatting

--- src/Attribute.java
+++ src/Attribute.java
@@ -1,3 +1,11 @@
+/*
+ * Attribute class
+ * 
+ * Creates Attribute objects from the data from the SQL database
+ * Attribute objects stored in ArrayList passed to XML Formatter
+ * Attribute stores data about each attribute retrieved from SQL database
+ * 
+ */
 
 public class Attribute {
 	
@@ -5,49 +13,42 @@ public class Attribute {
 	String alias;		// attribute's alias (attribute name upon query)
 	Group group;		// group object if the attribute is to be grouped
 	String tableName;	// tableName that the attribute belongs to
-
 	boolean compFlag;	// is this attribute compressed alone?
 	Attribute compTo;	// the attribute information for the single compression
-	
+
 	// empty constructor left for testing purposes
 	Attribute(){
 		
 	}
 	
-	// minimal constructor
-	Attribute(String name, String tableName){
+	// minimal constructor for Attribute object
+	Attribute(String name, String tableName) {
 		
-		this.name = name;
-		compFlag = false;
-		
+		this.name = name;	// initialize Attribute identifying name
+		compFlag = false;	// initialize compression flag to false
 	}
 	
-	// attribute constructor w/ name, and alias
+	// Attribute constructor with table name and alias
 	Attribute(String name, String tableName, String alias){
 		
-		this.name = name;
-		this.tableName = tableName;
-		this.alias = alias;
-		compFlag = false;
-		
+		this.name = name;				// initialize Attribute identifying name
+		this.tableName = tableName;		// initialize Attribute table name
+		this.alias = alias;				// initialize alias field for [attribute AS name]
+		compFlag = false;				// initialize compression flag to false
 	}
 	
 	// ------------------------------------------------------------
 	
-	// modifies the attribute to be a child of a group
-	public void addGroup(Group group){
+	// modifies the Attribute object to be a child of a group
+	public void addGroup(Group group) {
 		
-		this.group = group;
-		
+		this.group = group;		// adds the Group object to the Attribute object
 	}
 	
-	
-	// modifies the attribute to be compressed to the passed attribute
-	public void addCompression(Attribute compTo){
+	// modifies the Attribute object to be compressed to the passed Attribute
+	public void addCompression(Attribute compTo) {
 		
-		this.compFlag = true;
-		this.compTo = compTo;
-		
+		this.compFlag = true;	// sets the compression flag to true for the Attribute
+		this.compTo = compTo;	// sets the passed Attribute as the compression Attribute
 	}
-
 }
--- src/Group.java
+++ src/Group.java
@@ -1,10 +1,17 @@
+/*
+ * Group class
+ * 
+ * Group class used for grouping (DEFINITION #3)
+ * Creates Group objects as a result of grouping from the query
+ * Group objects stored as part of each Attribute object
+ * Group object stores data about the group as requested by the query
+ * 
+ */
 
-// group class used for grouping (DEFINITION #3)
 public class Group {
 	
 	String name;		// name of group <THIS-RIGHT-HERE, attr1, attr2>
 						// if this is UNNAMED (null) not grouped, just compressed
-	boolean compFlag;	// is this group compressed?
+	boolean compFlag;	// flags the group as compressed
 	Attribute compTo;	// the attribute information for the group compression
-	
 }
